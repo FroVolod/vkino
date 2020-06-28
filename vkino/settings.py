a40865f445b4174fe77cbc87b3388d57b81ea654
FroVolod <frol_off@meta.ua>
2020-06-28 09:25:06 +0300

    changed debug

--- vkino/settings.py
+++ vkino/settings.py
@@ -24,7 +24,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'wu#9mxu%up*!48^=urgcf4wi3&i(*sp#!gc$s&eq9hd$zc&0-2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
